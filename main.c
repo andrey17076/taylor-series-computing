6847cbc76d633615898e85e68cbd48b30aae49bd
Andrey Tatarenko <andrey17076@gmail.com>
2016-03-23 22:08:21 +0300

    init

--- main.c
+++ main.c
@@ -1,33 +1,17 @@
-#include <string.h>
+#include <stdio.h>
 #include <unistd.h>
 
-int num=0;
-char namebuf[20];
-char prefix[] = "/tmp/tmp";
-
-char *gentemp(void) {
-    int length;
-    pid_t pid;
-    pid=getpid();
-    strcpy(namebuf, prefix);
-    length = strlen(namebuf);
-    itoa(pid, &namebuf[length]);
-    strcat(namebuf,".");
-    length = strlen(namebuf);
-    do{
-        itoa(num++,&namebuf[length]);
-    } while (access(namebuf, F_OK) != -1);
-    return (namebuf);
-}
-
-int itoa(int i; char *string) {
-    int power, j;
-    j = i;
-    for (power = 1; j >= 10; j /= 10)
-        power *= 10;
-    for ( ; power > j; power/=10) {
-        *string ++= '0' + i / power;
-        i %= power;
-    }
-    *string = '\0';
+int main ()
+{
+    pid_t pid;       /* идентификатор процесса */
+    printf ("Пока всего один процесс\n");
+    pid = fork ();     /* Создание нового процесса */
+    printf ("Уже два процесса\n");
+    if (pid == 0) {
+        printf ("Это Дочерний процесс его pid=%d\n", getpid());
+        printf ("А pid его Родительского процесса=%d\n", getppid());
+    } else if (pid > 0)
+        printf ("Это Родительский процесс pid=%d\n", getpid());
+    else
+        printf ("Ошибка вызова fork, потомок не создан\n");
 }
